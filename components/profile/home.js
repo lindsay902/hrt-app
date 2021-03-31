b7e2db7649d181ee93694a06456fa7cb335084f8
Lindsay Margaret Lee <lindsay@Lindsays-MacBook-Air.local>
2021-03-30 22:19:56 -0500

    added community question buttons

--- components/profile/home.js
+++ components/profile/home.js
@@ -1,15 +1,14 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image, Button } from 'react-native';
 import { useFonts } from 'expo-font';
-import { Button } from 'react-native-paper';
 import EstrogenContextProvider from '../Context/HormoneLevels/estrogenLevel';
 import EstrogenHormoneLevels from '../Context/HormoneLevels/estrogenLevel';
 import TestosteroneContextProvider from '../Context/HormoneLevels/testosteroneLevel';
 import TestosteroneHormoneLevels from '../Context/HormoneLevels/testosteroneLevel';
 import PotassiumContextProvider from '../Context/HormoneLevels/potassiumLevel';
 import PotassiumHormoneLevels from '../Context/HormoneLevels/potassiumLevel';
-import SvgComponent from '../Community/Polls/symptomsPoll';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const Home = ({ navigation }) => {
   const [loaded] = useFonts({
@@ -46,17 +45,83 @@ const Home = ({ navigation }) => {
           <Text>Information!</Text>
         </View>
       </View>
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, marginTop: -100 }}>
         <Text style={styles.textStyle}>Community Questions</Text>
         <View
           style={{
             flexDirection: 'row',
-            justifyContent: 'space-between',
+            justifyContent: 'space-around',
+            flexWrap: 'wrap',
           }}
         >
-          <Text>Test1</Text>
-          <Text>Test2</Text>
-          <Text>Test3</Text>
+          <View>
+            <Image
+              style={styles.image}
+              source={require('../../assets/resources/polls/Symptoms.png')}
+            />
+            <TouchableOpacity style={styles.buttons}>
+              <Text
+                style={{
+                  color: 'black',
+                  fontSize: 15,
+                  fontWeight: 'bold',
+                }}
+              >
+                Participate
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Image
+              style={styles.image}
+              source={require('../../assets/resources/polls/MentalHealth.png')}
+            />
+            <TouchableOpacity style={styles.buttons}>
+              <Text
+                style={{
+                  color: 'black',
+                  fontSize: 15,
+                  fontWeight: 'bold',
+                }}
+              >
+                Participate
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Image
+              style={styles.image}
+              source={require('../../assets/resources/polls/Beauty.png')}
+            />
+            <TouchableOpacity style={styles.buttons}>
+              <Text
+                style={{
+                  color: 'black',
+                  fontSize: 15,
+                  fontWeight: 'bold',
+                }}
+              >
+                Participate
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.button}>
+            <Image
+              style={styles.image}
+              source={require('../../assets/resources/polls/Relationships.png')}
+            />
+            <TouchableOpacity style={styles.buttons}>
+              <Text
+                style={{
+                  color: 'black',
+                  fontSize: 15,
+                  fontWeight: 'bold',
+                }}
+              >
+                Participate
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </LinearGradient>
@@ -82,19 +147,25 @@ const styles = StyleSheet.create({
     height: 100,
   },
   buttons: {
-    paddingTop: 5,
-    paddingBottom: 5,
-    marginTop: 20,
-    marginLeft: 100,
-    marginRight: 100,
-    backgroundColor: '#2D2B2B',
+    backgroundColor: 'white',
     borderRadius: 10,
+    marginTop: -30,
+    alignItems: 'center',
+    marginRight: 10,
+    marginLeft: 10,
+    paddingTop: 5,
+    shadowColor: 'grey',
+    shadowOpacity: 10,
   },
   textStyle: {
     fontFamily: 'HandleeRegular',
     fontSize: 30,
     alignSelf: 'center',
     paddingBottom: 10,
+  },
+  image: {
+    width: 175,
+    height: 175,
   },
 });
 
